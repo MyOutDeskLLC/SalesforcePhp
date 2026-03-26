4498ca838bfb5ed91dfa9fded8d9bd86451bc03a
Michael Dyrynda <michael.dyrynda@lmg.broker>
2026-03-26 17:22:05 +1030

    fix: add missing implements HasBody to UpdateRecords
    Saloon v4 body traits assert that the request implements HasBody at
    boot time and throw BodyException if it does not. UpdateRecords used
    HasJsonBody but omitted the interface, making every PATCH request fail.

--- src/Requests/SObjects/UpdateRecords.php
+++ src/Requests/SObjects/UpdateRecords.php
@@ -2,11 +2,12 @@
 
 namespace myoutdeskllc\SalesforcePhp\Requests\SObjects;
 
+use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 use Saloon\Traits\Body\HasJsonBody;
 
-class UpdateRecords extends Request
+class UpdateRecords extends Request implements HasBody
 {
     use HasJsonBody;
 
